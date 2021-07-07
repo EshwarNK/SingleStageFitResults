8a3bfbc223b03d16fc42f0f263d6a71bcfb322e7
eshwar-kumar <eshwar.kumar@salesforce.com>
2021-07-07 00:00:43 -0400

    Adding latest deck changes

--- singlestagefitresults-deck/src/index.ts
+++ singlestagefitresults-deck/src/index.ts
@@ -1,6 +1,8 @@
 import { IDeckPlugin } from '@spinnaker/core';
 import { deployFetchTestIdDisplayFitResults } from './DeployFetchTestIdDisplayFitResults';
+// import { initialize } from './DeployFetchTestIdDisplayFitResultsStageConfig';
 
 export const plugin: IDeckPlugin = {
+  // initialize,
   stages: [deployFetchTestIdDisplayFitResults],
 };
